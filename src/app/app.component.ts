cc385e533989a23311779233d271ba7e8db249e2
ialmanza <110355705+ialmanza@users.noreply.github.com>
2024-06-02 13:27:06 -0300

    uso de supabase para auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { SupabaseService } from './services/supabase.service';
+import { AccountComponent } from "./components/account/account.component";
+import { AuthComponent } from "./components/auth/auth.component";
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [AccountComponent, AuthComponent, CommonModule],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'supabase-angular';
+  session = this.supabase.session
+
+  constructor(private readonly supabase: SupabaseService) {
+
+  }
+
+  ngOnInit() {
+    this.supabase.authChanges((_, session) => (this.session = session))
+  }
 }
